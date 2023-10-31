df551754d3db3f2819a584ddfd67fcd2ef105e4f
saaddighi <saaddighi20@gmail.com>
2023-10-31 21:08:30 +0000

    ok

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = (str[str.index('object') : str.index('lan')] + (str[str.index('with'):str.index('very')]) + (str[0:6]))
+str = (str[str.index('object'):str.index('lan')] + (str[str.index('with'):str.index('very')]) + (str[0:6]))
 print(str)
