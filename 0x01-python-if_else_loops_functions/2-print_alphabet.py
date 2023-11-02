caaafc2540c6099f67351ef08c26d42f8fe1124c
saaddighi <saaddighi20@gmail.com>
2023-11-02 16:34:02 +0000

    ok

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-output = ' '.join([chr(i) for i in range(ord('a'), ord('z') + 1)])
-print("{}".format(output), end=' ')
+output = ' '.join([chr(i) for i in range(ord('a'),ord('z') + 1)])
+print("{}".format(output.replace(' ','')))
