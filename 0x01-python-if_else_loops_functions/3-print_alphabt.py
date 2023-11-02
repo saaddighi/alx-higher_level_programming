07e182adaa255e7b5821c8272c9e9f4274d881e0
saaddighi <saaddighi20@gmail.com>
2023-11-02 17:08:43 +0000

    ok

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for i in range(ord('a'), ord('z') + 1):
+    end = '' if i == ord('z') else ''
+    print("{}".format(chr(i)).replace('e','').replace('q',''), end=end, flush=True)
