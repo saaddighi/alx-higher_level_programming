50d6f47cdbf6d774b700a208b0b7850b73fc3957
saaddighi <saaddighi20@gmail.com>
2023-11-08 17:24:10 +0000

    ok

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,10 +2,8 @@
 a = 1
 b = 2
 if __name__ == '__main__':
-    def add(a,b):
+    def add(a, b):
         
        a = 1
        b = 2
        return print("{} + {} = {}".format(a, b,a+b))
-add(a,b)
-
