fd10ab8969e89f3546eed9f78ee8901a4b53044a
saaddighi <saaddighi20@gmail.com>
2023-11-09 15:49:58 +0000

    ok

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    a = 10
+    b = 5
+    
+    def add(a, b):
+        return print(a + b)
+    def sub(a,b):
+        return print(a - b)
+    def div(a,b):
+        return print(a / b)
+    def mul(a,b):
+        return print(a * b)
+    
