5a207340f26b121885839b069e05a735fce5d12a
saaddighi <saaddighi20@gmail.com>
2023-11-10 16:57:16 +0000

    ok

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-def taks(argument):
-    a = len(argument)
-    if a == 1:
-        a = f'{a} argument:'
-    elif a == 0 :
-        a = f'{a} arguments.'
-    else:
-        a = f'{a} arguments:'
-    for i in argument :
-        g = argument.index(i) + 1
-        t = str(g)
-        s = f'{t} : {argument}'
-    return print(f"{a} \n {s} \n")  
+
+if __name__ == '__main__':
+    
+    
+    def taks(argument):
+    
+        a = len(argument)
+        if a == 1:
+            a = f'{a} argument:'
+        elif a == 0 :
+            a = f'{a} arguments.'
+        else:
+            a = f'{a} arguments:'
+        for i in argument :
+            g = argument.index(i) + 1
+            t = str(g)
+            s = f'{t} : {argument}'
+        return print(f"{a} \n {s} \n")  
         
