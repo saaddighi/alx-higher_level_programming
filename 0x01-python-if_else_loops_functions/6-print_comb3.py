7c1bf53f8372a5d789cf9da3256d23fdca2d2df4
saaddighi <saaddighi20@gmail.com>
2023-11-16 16:59:40 +0000

    ok git push

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for i in range(100):
         i = f'0{i}'
     
     if str(i) < str(i)[::-1]:
-        print(i,end=', ' if int(i) < 89 else '\n')
+        print('{}'.format(i),end=', ' if int(i) < 89 else '\n')
