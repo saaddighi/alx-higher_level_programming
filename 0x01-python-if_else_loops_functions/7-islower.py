6993ea5d0744709c737ffd6fdaa95b4056f35869
saaddighi <saaddighi20@gmail.com>
2023-11-16 19:40:02 +0000

    ok

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-p = []
 def islower(c):
-    for i in range(ord('a'), ord('z')+1):
-        p.append(chr(i))
-    if c in p:
-        return True
-    else:
-        return False
+    return ord('a') <= ord(c) <= ord('z')
