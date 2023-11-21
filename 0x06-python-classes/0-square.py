cc2e359c54cf2a32a028bbf159f56f1c37a8cd5a
saaddighi <saaddighi20@gmail.com>
2023-11-21 17:18:55 +0000

    ok

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 class Square:
-    pass
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+    """
+    Represents a square in geometry.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the Square class.
+        """
+        pass
