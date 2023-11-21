f327e6da8fa5d2f76d2ab6f9ac92f308b02e7cb5
saaddighi <saaddighi20@gmail.com>
2023-11-21 18:27:27 +0000

    ok

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A module for square"""
+
+
+class Square:
+    """Represent a square."""
+    
+    def __init__(self,size):
+        """size of the square is what define it"""
+        self.size = size
+        
