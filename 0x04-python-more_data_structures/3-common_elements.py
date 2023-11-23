b71fee2d9e494792463abf1847655c3c32e3a4ef
saaddighi <saaddighi20@gmail.com>
2023-11-22 16:53:26 -0800

    ok

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,22 +1,4 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    # Initialize an empty set to store common elements
-    common_set = set()
-
-    # Iterate through elements in set_1
-    for element in set_1:
-        # Check if the element is also in set_2
-        if element in set_2:
-            # Add the common element to the common_set
-            common_set.add(element)
-
-    return common_set
-
-# Example usage:
-set1 = {1, 2, 3, 4, 5}
-set2 = {4, 5, 6, 7, 8}
-
-result = common_elements(set1, set2)
-print(result)
-
+    return set_1.intersection(set_2)
