dd52c662f9c7ba5dfe50155be5d5a1817521bd8d
saaddighi <saaddighi20@gmail.com>
2023-11-29 12:58:45 +0000

    ok

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -7,7 +7,7 @@ Defines a Rectangle class.
 class Rectangle:
     """Public class attribute"""
     number_of_instances = 0
-    
+ 
     """Rectangle class defined by width and height."""
 
     def __init__(self, width=0, height=0):
@@ -16,8 +16,6 @@ class Rectangle:
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
-        
-        
 
     @property
     def width(self):
