e5e753b3ad07bb1c417c9b26e9d377e72af0363f
saaddighi <saaddighi20@gmail.com>
2023-12-04 16:56:15 +0000

    ok

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -90,7 +90,7 @@ class Rectangle:
         """
         print('Bye rectangle...')
         Rectangle.number_of_instances -= 1
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Finds the biggest Rectangle based on the area
