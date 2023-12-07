179ca1bc72320dbf4a93f54a04c5eb0373ceba2b
saaddighi <saaddighi20@gmail.com>
2023-12-07 11:30:01 +0000

    ok

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""a function that returns a list of
+available attributes and methods of an object
+"""
 def lookup(obj):
-    """a function that returns a list of
-    available attributes and methods of an object"""
+    """we creat a list variable then append the 
+    output of dir(obj) in it"""
     l= []
     l.append(dir(obj))
     return l
