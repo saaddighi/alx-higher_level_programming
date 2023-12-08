191d255788db854f92d0f1a5e4225300c5f944c0
saaddighi <saaddighi20@gmail.com>
2023-12-08 12:12:43 +0000

    ok

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def  add_integer(a, b=98):
+    if not isinstance(a, int) or not isinstance(a, float):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, int) or not isinstance(b, float):
+        raise TypeError('b must be an integer')
+    a = int(a)
+    b = int(b)
+    result = a + b
+    return result
