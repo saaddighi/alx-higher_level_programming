69ee1fba364a8c531c5bdb253c59fd45b25d17d2
saaddighi <saaddighi20@gmail.com>
2023-12-13 14:52:28 +0000

    ok

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+    for i in matrix:
+        while matrix.index(i) < len(matrix):
+            if len(i) != len(matrix[matrix.index(i)+1]):
+                raise type('Each row of the matrix must have the same size')
+    if div not in [int, float]:
+        raise TypeError('div must be a number')
+    elif div == 0:
+        ZeroDivisionError('division by zero')  
+
+    for i in matrix: 
+        for d in i:
+            if type(d) not in [int,float]:
+                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')            
+        return print(d / div)
+
