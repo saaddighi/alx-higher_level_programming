ac3f5112520e596bf35a84f63d14b7e125ef73bb
saaddighi <saaddighi20@gmail.com>
2023-12-20 12:56:02 +0000

    ok

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""a function that prints a square"""
+
+
+def print_square(size):
+    """if size is not an integer raise error"""
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    """if size is less then 0 raise an error"""
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    """if size is a float and is less than 0 raise error"""
+    if isinstance(size, float) == True and size < 0:
+        raise TabError('size must be an integer')
+    for i in range(size):
+        p = '#'*size
+        print(p)
