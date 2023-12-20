0d81992d9e8cad84d1052a4a42efee5ff6269b05
saaddighi <saaddighi20@gmail.com>
2023-12-20 11:55:12 +0000

    ok

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
+"""a function that return the first and last name"""
+
 def say_my_name(first_name, last_name=""):
+    """raise exeption if first name not a string"""
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
-    
+    """raise exeption if last name not a string"""
     if not isinstance(last_name, str):
         raise TypeError('last_name must be a string')
 
