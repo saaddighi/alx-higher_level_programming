be31e9ebd6c5c22ad903d262baa1aaa81dbcb047
saaddighi <saaddighi20@gmail.com>
2024-01-16 13:04:29 -0800

    ok

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- A script that prints the full description of the table first_table from the database hbtn_0c_0 in my MySQL server.
-SHOW COLUMNS FROM first_table;
+-- A script that prints the full description of the table first_table from the database hbtn_0c_0 in my 
+SHOW CREATE TABLE `first_table`;
